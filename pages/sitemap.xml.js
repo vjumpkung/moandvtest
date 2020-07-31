b0d746301e582305ae06163cf23495b8bec6820f
Chanrich Pisitjing <vjumpkung@gmail.com>
2020-07-31 22:28:25 +0700

    fix sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,5 +1,4 @@
 const sitemapXml = () => {
-  const { postsXml } = blogPostsXml();
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
@@ -10,7 +9,6 @@ const sitemapXml = () => {
       <loc>https://moandv.com/about</loc>
       <priority>0.8</priority>
     </url>
-    ${postsXml}
   </urlset>`;
 };
 
